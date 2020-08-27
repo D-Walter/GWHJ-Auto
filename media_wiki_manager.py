2a0c4fbcaa92252550f80b3a7bf2e8ad9954b819
Dennis Walter <walter_d@qq.com>
2020-08-28 02:54:29 +0800

    Revert "Revert "normal commit""
    This reverts commit 2300e5e668946b3abeb6279f8676f80745946e3d.

--- media_wiki_manager.py
+++ media_wiki_manager.py
@@ -5,6 +5,7 @@ import time
 from time import sleep
 from mwclient import Site, listing, page
 from mwclient.page import Page
+import global_utils
 
 
 class MediaWikiManager:
@@ -15,20 +16,31 @@ class MediaWikiManager:
     __operation_index_limitation: int = 5
     __operation_index_mutex = threading.Lock()
 
-    def get_items_in_category(self, in_category, sender="UNKNOWN"):
-        MediaWikiManager.__try_doing__(self.__walk_category__,
-                                       lambda: self.__write_log__(""))
-        return self.__walk_category__(in_category, [])
+    def get_items_in_categories(self, in_categories: list, sender="UNKNOWN"):
+        temp_list = []
+        for c in in_categories:
+            cur_items = self.get_items_in_category(c, sender)
+            if cur_items is None:
+                raise Exception(f"Get items in categories failed on '{c}'")
+            temp_list[0:0] = cur_items
+        return temp_list
 
-    def __walk_category__(self, category_name: str, c_list: list) -> list:
-        site_service = self.__get_site_service__(need_logged=False)
-        try:
+    def get_items_in_category(self, in_category, sender="UNKNOWN") -> list or None:
+        return self.__walk_category__(in_category, sender)
+
+    def __walk_category__(self, category_name: str, sender: str = "None") -> None or list:
+        my_operation_index = self.__ask_for_operation_index__()
+        site_service = self.__get_site_service__(f'OPERATION[WALK] from USER[{sender}]', False)
+        ret = None
+
+        def __walk_category_rcr__(_category_name=category_name, c_list=[]) -> None:
+            nonlocal ret, site_service
             temp_items = []
-            for obj in site_service.categories[category_name]:
+            for obj in site_service.categories[_category_name]:
                 if isinstance(obj, listing.Category):
                     c_name = obj.name
                     c_name = c_name.replace('Category:', '')
-                    self.__walk_category__(c_name, c_list)
+                    __walk_category_rcr__(c_name, c_list)
                 elif isinstance(obj, page.Page):
                     name = obj.name
                     if name not in c_list:
@@ -37,10 +49,20 @@ class MediaWikiManager:
                             temp_items.append(name)
                             print('add page:', name)
             c_list[0:0] = temp_items
-            return c_list
-        except PermissionError:
-            sleep(30)
-            return []
+            ret = c_list
+
+        operation_details_text = f"OPERATION[WALK] to CATEGORY[{category_name}] from USER[{sender}](OI[{my_operation_index}])"
+        result = MediaWikiManager.__try_doing__(__walk_category_rcr__,
+                                                lambda e, c: self.__write_log__(
+                                                    f"{operation_details_text} failed, this is the {c}th failure(s)"
+                                                    + os.linesep + e))
+        if result is None:
+            self.__write_log__(
+                f"{operation_details_text} completed")
+        else:
+            self.__write_log__(
+                f"{operation_details_text} failed[{result}]")
+        return ret
 
     def get_page(self, page_name: str, sender="UNKNOWN") -> Page or None:
         return self.__operate_page__('GET', page_name, sender=sender)
@@ -65,10 +87,7 @@ class MediaWikiManager:
         return ret
 
     def __write_log__(self, message):
-        with open(os.path.join('Logs', 'HuijiManager.log'), 'a') as fs:
-            fs.write(
-                f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}][{'ONLINE' if self.__site_service.logged_in else 'OFFLINE'}]{message}{os.linesep}")
-            fs.flush()
+        global_utils.write_log(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}][{'ONLINE' if self.__site_service.logged_in else 'OFFLINE'}]{message}")
 
     def __get_site_service__(self, sender='UNKNOWN', need_logged=True) -> Site:
         if need_logged and not self.__site_service.logged_in:
@@ -110,17 +129,18 @@ class MediaWikiManager:
             'CREATE': create_page,
             'DELETE': delete_page
         }
+        operation_details_text = f"OPERATION[{operation_type}] to PAGE[{page_name}] from USER[{sender}](OI[{my_operation_index}])"
         result = MediaWikiManager.__try_doing__(page_operations_funcs[operation_type],
                                                 lambda e, c: self.__write_log__(
-                                                    f"OPERATION[{operation_type}] to PAGE[{page_name}] from USER[{sender}] failed (OI[{my_operation_index}]), this is the {c}th failure(s)"
+                                                    f"{operation_details_text} failed, this is the {c}th failure(s)"
                                                     + os.linesep + e),
                                                 retry_times, retry_interval)
         if result is None:
             self.__write_log__(
-                f"OPERATION[{operation_type}] to PAGE[{page_name}] from USER[{sender}](OI[{my_operation_index}]) completed")
+                f"{operation_details_text} completed")
         else:
             self.__write_log__(
-                f"OPERATION[{operation_type}] to PAGE[{page_name}] from USER[{sender}](OI[{my_operation_index}]) failed[{result}]")
+                f"{operation_details_text} failed[{result}]")
         return tgt_page
 
     @staticmethod
@@ -141,7 +161,7 @@ class MediaWikiManager:
                 sleep(retry_interval)
         return None
 
-    def __init__(self, account, password, host="gw2.huijiwiki.com"):
+    def __init__(self, account, password, host):
         self.__account = account
         self.__password = password
-        self.__site_service = Site(host)
+        self.__site_service = Site(host, path='/')
